38efac8286bc9f2d2bb013131c234e0f86a97853
Johnson-Richie Louis <johnson.richie.louis@gmail.com>
2019-01-25 21:09:11 +0100

    Configure project add cocoapods dependencies

---
+++ Aliexpress/SwiftGenTemplates/swiftgen.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+BASE_DIRECTORY="Aliexpress"
+GENERATED_DIRECTORY="$BASE_DIRECTORY/Generated"
+RESSOURCES_DIRECTORY="$BASE_DIRECTORY/Ressources"
+
+mkdir -p ${GENERATED_DIRECTORY}
+mkdir -p ${RESSOURCES_DIRECTORY}
+
+./Pods/SwiftGen/bin/swiftgen storyboards \
+    -t scenes-swift4 $BASE_DIRECTORY \
+    -o $GENERATED_DIRECTORY/Storyboards.swift \
+    --param AppModule=Aliexpress
+./Pods/SwiftGen/bin/swiftgen strings \
+    -t structured-swift4 \
+    $RESSOURCES_DIRECTORY/Localizable/en.lproj/Aliexpress-localizable.strings \
+    --output $GENERATED_DIRECTORY/GeneratedLocalizableStrings.swift
+./Pods/SwiftGen/bin/swiftgen xcassets \
+    -t swift4 \
+    $RESSOURCES_DIRECTORY/Assets.xcassets \
+    --output $GENERATED_DIRECTORY/GeneratedAssets.swift
