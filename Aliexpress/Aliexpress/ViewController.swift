1c7ede703f38047317cac6531abc146d3ef3b639
Johnson-Richie Louis <johnson.richie.louis@gmail.com>
2019-01-25 19:21:04 +0100

    Create xcode project - intial commit

---
+++ Aliexpress/Aliexpress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aliexpress
+//
+//  Created by Johnson-Richie Louis on 25/01/2019.
+//  Copyright © 2019 Johnson-Richie Louis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
